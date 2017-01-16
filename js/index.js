50b2443eecebdd645adffb55e66226f9dd57d2db
Adam Krueger <akrueger@einsteinmedical.com>
2017-01-16 12:41:56 -0800

    updated the template files to include database connections via the form.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,30 @@
+// Makes nav pop up after scrolling past the hero header
+// $(window).scroll(function() {    
+//     var scroll = $(window).scrollTop();
+//     if (scroll < 200) {
+//       $(".navbar").removeClass("scroll-hide");
+//     }
+//     if (scroll >= 200 && scroll <=599) {
+//       $(".navbar").addClass("scroll-hide");
+//     }
+//     if (scroll >= 600) {
+//       $(".navbar").addClass("scroll-fix" );
+//     }
+//     if (scroll < 600) {
+//       $(".navbar").removeClass("scroll-fix");
+//     }
+// });
+
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
-    if (scroll < 200) {
-      $(".navbar").removeClass("scroll-hide");
+    var headerHeight = $(".hero").innerHeight();
+    if (scroll < headerHeight) {
+      $(".navbar").removeClass("nav-scroll");
+      $(".navbar").addClass("nav-top");
     }
-    if (scroll >= 200 && scroll <=599) {
-      $(".navbar").addClass("scroll-hide");
-    }
-    if (scroll >= 600) {
-      $(".navbar").addClass("scroll-fix" );
-    }
-    if (scroll < 600) {
-      $(".navbar").removeClass("scroll-fix");
+    if (scroll >= headerHeight) {
+      $(".navbar").removeClass("nav-top");
+      $(".navbar").addClass("nav-scroll");
     }
 });
+
