111e9c561c8d04a8f302b6593eec64c6673538f4
Adam Krueger <akrueger@einsteinmedical.com>
2017-03-13 19:24:18 -0700

    updated the footer with a different style for the page nav

--- includes/site_footer.php
+++ includes/site_footer.php
@@ -1,31 +1,28 @@
 <div class="stripe footer">
   <div class="container">
-    <div class="flex-grid">
-      <div class="col">
-        <h3><strong>Navigation</strong></h3>
-        <?php include 'includes/site_nav.php'; ?>
-      </div>
-      <div class="col">
-        <h3><strong>The Nitty Gritty</strong></h3>
-        <ul>
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Contact Us</a></li>
-          <li><a href="#">Terms of Service</a></li>
-          <li><a href="#">Privacy Policy</a></li>
-          <li><a href="#">Site Map</a></li>
-        </ul>
-      </div>
-      <div class="col">
-        <h3><strong>Contact Info</strong></h3>
-        <p>
-          Phone: (123) 456-7890
-          <br>1234 100th Street
-          <br>San Diego, CA 90123
-          <br>Designed &amp; Developed 
-          <br>by Adam Krueger
-          <br> &copy; Copyright 2016-2017
-        </p>
-      </div>
+
+    <div class="footer-navigation">
+      <?php include 'includes/site_nav.php'; ?>
+      <ul>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">Terms of Service</a></li>
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Site Map</a></li>
+      </ul>
     </div>
+
+    <div class="footer-contact">
+      <h3><strong>Contact Info</strong></h3>
+      <p>
+        Phone: (123) 456-7890
+        <br>1234 100th Street
+        <br>San Diego, CA 90123
+        <br>Designed &amp; Developed 
+        <br>by Adam Krueger
+        <br> &copy; Copyright 2016-2017
+      </p>
+    </div>
+
   </div>
 </div>
